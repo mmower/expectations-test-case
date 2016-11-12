ee1a753ae66323376b7faf43159e79b2a6c068c0
Matt Mower <self@mattmower.com>
2016-11-12 10:36:31 +0000

    Added the output of running the test
    When the macro is not inside a call to 'expect' the exception is thrown. However in the context of 'expect' it doesn't seem to be.

--- test/test_case/core_test.clj
+++ test/test_case/core_test.clj
@@ -8,3 +8,19 @@
   (catch Exception ex (println "Got an exception")))
 
 (expect Exception (macroexpand-1 '(unstable)))
+
+;Alia:test-case matt$ lein autoexpect
+;
+;*********************************************
+;*************** Running tests ***************
+;:reloading (test-case.core test-case.core-test)
+;Got an exception
+;
+;failure in (core_test.clj:10) : test-case.core-test
+;(expect Exception (macroexpand-1 '(unstable)))
+;
+;           (macroexpand-1 (quote (unstable))) did not throw Exception
+;
+;Ran 1 tests containing 1 assertions in 18 msecs
+;1 failures, 0 errors.
+;Tests completed at 10:34:30.591
